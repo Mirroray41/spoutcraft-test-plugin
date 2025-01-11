85d0e842053c65d383f1c52ddc4ebbc8c99cdebc
Mirroray41 <87991171+Mirroray41@users.noreply.github.com>
2025-01-11 12:44:23 +0100

    texture test 3

--- src/main/java/net/zapp/ExampleBlock.java
+++ src/main/java/net/zapp/ExampleBlock.java
@@ -7,7 +7,7 @@ import org.getspout.spoutapi.material.block.GenericCuboidCustomBlock;
 import static net.zapp.Main.GIT_ASSETS;
 
 public class ExampleBlock extends GenericCuboidCustomBlock {
-    static Texture texture = new Texture(Main.get(), GIT_ASSETS + "texture_test.png", 6, 6, 6);
+    static Texture texture = new Texture(Main.get(), GIT_ASSETS + "texture_test.png", 16, 16, 16);
     public ExampleBlock() {
         super( Main.get(), "exampleblock", 85, (GenericCuboidBlockDesign) new GenericCuboidBlockDesign(Main.get(), texture, 0, (float) 0.3125, (float) 0.6875, (float) 0.3125, (float) 0.6875, (float) 0.3125, (float) 0.6875).setRenderPass(1));
         setName("Example Block");
