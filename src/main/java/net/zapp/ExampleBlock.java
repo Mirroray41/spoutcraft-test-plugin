337b46a3218a3874f2b32c4085048199ef2eda7e
Mirroray41 <87991171+Mirroray41@users.noreply.github.com>
2025-01-12 11:43:30 +0100

    Custom block shapes kinda working

--- src/main/java/net/zapp/ExampleBlock.java
+++ src/main/java/net/zapp/ExampleBlock.java
@@ -1,15 +1,14 @@
 package net.zapp;
 
+import org.getspout.spoutapi.block.design.BlockDesign;
 import org.getspout.spoutapi.block.design.GenericCuboidBlockDesign;
-import org.getspout.spoutapi.block.design.Texture;
 import org.getspout.spoutapi.material.block.GenericCuboidCustomBlock;
 
-import static net.zapp.Main.GIT_ASSETS;
 
 public class ExampleBlock extends GenericCuboidCustomBlock {
-    static Texture texture = new Texture(Main.get(), GIT_ASSETS + "texture_test.png", 16, 16, 16);
-    public ExampleBlock() {
-        super( Main.get(), "exampleblock", 85, (GenericCuboidBlockDesign) new GenericCuboidBlockDesign(Main.get(), texture, 0, (float) 0.3125, (float) 0.6875, (float) 0.3125, (float) 0.6875, (float) 0.3125, (float) 0.6875).setRenderPass(1));
+    public ExampleBlock(BlockDesign design) {
+        super(Main.get(), "exampleblock", 20, (GenericCuboidBlockDesign) design);
         setName("Example Block");
+        setOpaque(false);
     }
 }
--- src/main/java/net/zapp/Main.java
+++ src/main/java/net/zapp/Main.java
@@ -2,6 +2,9 @@ package net.zapp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.getspout.spoutapi.block.design.BlockDesign;
+import org.getspout.spoutapi.block.design.GenericCuboidBlockDesign;
+import org.getspout.spoutapi.block.design.Texture;
 
 public class Main extends JavaPlugin {
 
@@ -18,8 +21,12 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // The item needs to be initialised somewhere. Add as variable to this class so it can be referenced to add to inventories
+        Texture texture = new Texture(Main.get(), GIT_ASSETS + "texture_test.png", 8, 8, 8);
+        BlockDesign design = new GenericCuboidBlockDesign(Main.get(), texture, 0, (float) 0.25, (float) 0.75, (float) 0.25, (float) 0.75, (float) 0.25, (float) 0.75);
+        design.setRenderPass(1);
+        design.setBoundingBox((float) 0.25, (float) 0.25, (float) 0.25, (float) 0.75, (float) 0.75, (float) 0.75);
         EXAMPLE_ITEM = new ExampleItem();
-        EXAMPLE_BLOCK = new ExampleBlock();
+        EXAMPLE_BLOCK = new ExampleBlock(design);
     }
 
     public static Main get() {
